07b6475086d79586d3e399cf32990b5ff8731029
Alan Christian A. S. <wizdoux@proton.me>
2025-01-21 23:42:18 -0300

    Update pfetch.sh

--- pfetch.sh
+++ pfetch.sh
@@ -1,1 +1,17 @@
-clear; EDITOR="neovim" HOSTNAME="nix" PF_ASCII="arch" PF_INFO="ascii title os kernel uptime shell editor de palette" pfetch;
+# One liner...
+# clear; EDITOR="neovim" HOSTNAME="nix" PF_ASCII="arch" PF_INFO="ascii title os kernel uptime shell editor de palette" pfetch;
+
+# Full code
+# Clear the terminal screen
+clear
+
+# Set environment variables
+EDITOR="neovim"        # Preferred text editor
+HOSTNAME="nix"         # Hostname of the machine
+PF_ASCII="arch"        # ASCII art to display
+
+# Information to display
+PF_INFO="ascii title os kernel uptime shell editor de palette"
+
+# Execute the pfetch command with the specified environment variables
+pfetch
