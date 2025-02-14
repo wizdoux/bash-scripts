a09d653781b8a5deb8a15cd8a48b0de568eb5998
Alan Christian A. S. <wizdoux@proton.me>
2025-02-14 14:52:54 -0300

    adicionar configurações do i3, e um script de lockscreen

--- pfetch.sh
+++ pfetch.sh
@@ -1,1 +1,1 @@
-echo ""; EDITOR="neovim" HOSTNAME="nix" PF_ASCII="arch" PF_INFO="ascii title os shell editor de palette" pfetch;
+echo ""; EDITOR="neovim" XDG_CURRENT_DESKTOP="i3wm" HOSTNAME="nix" PF_ASCII="linux" PF_INFO="ascii title os shell editor de palette" pfetch;
