e28ef38bf8c1a3bc8a93c243745f97942a2b07f2
Alan Christian A. S. <wizdoux@proton.me>
2025-02-15 14:54:05 -0300

    Update pfetch.sh

--- pfetch.sh
+++ pfetch.sh
@@ -1,1 +1,1 @@
-echo ""; EDITOR="neovim" XDG_CURRENT_DESKTOP="i3wm" HOSTNAME="nix" PF_ASCII="linux" PF_INFO="ascii title os shell editor de palette" pfetch;
+echo ""; EDITOR="neovim" PF_ASCII="linux" PF_INFO="ascii title os shell editor de palette" pfetch;
